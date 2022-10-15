02047256c0714479c8751c2e773f8b6fa8f249e5
md-shakil-hosen <shakilinfo91@gmail.com>
2022-10-15 15:25:02 +0600

    Array

--- array.js
+++ array.js
@@ -21,15 +21,39 @@ text1 += "</ul>";
 
 //Adding Array Elements
 const fruits2 = ["Banana", "Orange", "Apple", "Mango"];
-document.getElementById('demo').innerHTML=fruits2;
+//document.getElementById('demo').innerHTML=fruits2;
 function myFruits(){
     fruits2.push("Shakil ");
-    document.getElementById('demo').innerHTML=fruits2;
+   // document.getElementById('demo').innerHTML=fruits2;
 }
 
 //JavaScript Array Methods
 const fruits4 = ["Banana", "Orange", "Apple", "Mango"];
-document.getElementById("array").innerHTML=fruits4.join(" * ");
-document.getElementById("pop").innerHTML=fruits4.pop(); // Pop just remove last element in array
-
+//document.getElementById("array").innerHTML=fruits4.join(" * ");
 //JavaScript Array pop()
+//document.getElementById("pop").innerHTML=fruits4.pop(); // Pop just remove last element in array
+
+// JavaScript array push()
+const fruits5=["Banana","Orange","Apple"];
+//document.getElementById("pop").innerHTML=fruits5.shift();
+
+//JavaScript Array shift()
+const fruits6=["Banana","Orange","Apple"];
+//document.getElementById("pop").innerHTML=fruits5.unshift();
+
+//JavaScript Array length
+const fruits7=["Banana","Orange","Apple"];
+//document.getElementById('arrayLength').innerHTML=fruits7.length('Mango');
+
+//
+const fruits8=["Banana","Orange","Apple"];
+delete fruits8[2];
+
+//JavaScript Array slice()
+const fruits9=["Banana","Orange","Apple"];
+
+//JavaScript Sorting Arrays
+const fruits10=["Banana","Orange","Apple"]; // Last to first sort
+
+//Reversing an Array
+console.log(fruits9.sort());
