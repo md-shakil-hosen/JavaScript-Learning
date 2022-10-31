4f0edb66702c4386d50e645990e5002d1256ee36
md-shakil-hosen <shakilinfo91@gmail.com>
2022-10-31 19:44:51 +0600

    Adding and Deleting Element

--- JavascriptDOM.js
+++ JavascriptDOM.js
@@ -1,1 +1,19 @@
-mvcmcbvmcb
+//1. JavaScript DOM
+//---------------------Finding HTML Elements--------------------
+//এখানে যে Object Modiul  গুলো দেওয় আছে তা দ্বারা আমরা সরাসরি Script থেকে ডাটাসমূহ HTML এ দেখাতে পারবো । 
+
+// 2.JavaScript - HTML DOM Methods
+//  -DOM-এ, সমস্ত HTML উপাদানকে অবজেক্ট হিসেবে সংজ্ঞায়িত করা হয় 
+//  -The HTML DOM (Document Object Model)
+//  -JavaScript HTML DOM Document
+//  -document.getElementById(id) - এইটটিএমল এর মধ্যে যে, যে আইডি আছে তা দেখতে হলে, getElementById
+//  -document.getElementsByTagName(name) - কোন ট্যাগ খোঁজার জন্য এটি ব্যবহার করা হয় ।
+//  -document.getElementsByClassName(name) - কোন ট্যাহ এর ক্লাস নেমকে ধরে কাজ করার জন্য 
+
+
+// ------------------Adding and Deleting Elements--------------------
+//  কোন এলিমেন্ট যুক্ত করার জন্য ঃ 
+// ১. document.createElement(element) - কোন নতুন এলিমেন্ট যুক্ত করার জন্য  ব্যবহার করা হয় ।
+// ২. document.removeChild(element) -  কোন এলিমেন্ট ডিলিট করার জন্য ব্যবহার করা হয় ।
+// ৩. document.appendChild(element) - এইচটিএমল এলিমেন্টের মধ্যে কোন নতুন এলিমেন্ট যুক্ত করতে এটি ব্যবহার করা হয় । 
+// ৪. -document.replaceChild(new, old) - এইটএমএল এর মধ্যে কোন একটি এলিমেন্টের জায়গায় অন্য কোন নতুন এলিমেন্ট যুক্ত করার জন্য এটি ব্যবহার করা হয় । 
