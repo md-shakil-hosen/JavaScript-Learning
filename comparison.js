f1667daeec20e28f5bef5d90fb788de8e1be0094
md-shakil-hosen <shakilinfo91@gmail.com>
2022-11-02 09:45:40 +0600

    comparisom

---
+++ comparison.js
@@ -0,0 +1,11 @@
+// && And = দুইটা শর্তই পূরণ করতে হবে ।
+// || OR =  যেকোনো একটি শর্ত পূরর্ণ করতে হবে ।
+
+var yourNumber = 20;
+var myNumber = 10;
+
+console.log(yourNumber > myNumber);
+console.log(yourNumber < myNumber);
+console.log(yourNumber != myNumber);
+
+
