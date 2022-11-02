72eefde5343c7a62196b85981a03249273378072
md-shakil-hosen <shakilinfo91@gmail.com>
2022-11-02 08:40:26 +0600

    push, pop, shift, unshift

---
+++ array.js
@@ -0,0 +1,5 @@
+var friendsAge = [25, 26, 14, 23, 14, 25, 12];
+console.log(friendsAge.length);
+console.log(friendsAge[5]); //এ্যারের পজিশন দেখার জন্য এইভাবে দেখাতে হবে ।
+friendsAge[3] = 20;// এ্যারের মধ্যে থেকে কোন একটি  এ্যারের মান পরিবর্তন করতে এভাবে লিখতে হবে ।
+console.log(friendsAge) 
---
+++ push-pop.js
@@ -0,0 +1,9 @@
+var friendsName = ["Shakil", " Shakib", " Takib", "sabik"];
+friendsName.push("babul");
+console.log(friendsName);
+friendsName.pop();
+console.log(friendsName);
+friendsName.shift();
+console.log(friendsName);
+friendsName.unshift("Shakil");
+console.log(friendsName);
