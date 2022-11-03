547aebb670051e453577bab6bcd3f01179779e05
md-shakil-hosen <shakilinfo91@gmail.com>
2022-11-03 17:07:11 +0600

    well

---
+++ Recap-loop.js
@@ -0,0 +1,33 @@
+// While Loop
+var i = 1;
+while(i<7){
+    i++; 
+    console.log(i);
+}
+
+
+for ( var i = 1; i <= 45; i++){
+    console.log(i);
+}
+
+// array Loop 
+
+var numbers = [25, 12, 56, 23, 14, 26, 45, 78, 45,69,23,45,26,25];
+for( var i = 0; i <numbers.length; i++){
+    var elements = numbers[i];
+    console.log(elements);
+}
+
+// Loop through
+
+var scores = [4, 45, 65, 23, 145, 10,26,12,45,56,25];
+
+while (i < scores.length){
+    console.log(scores[i]);
+}
+
+var scores = [4, 45, 65, 23, 145, 10,26,12,45,56,25];
+for( var i = 0; i <scores.length; i++){
+    var items = scores[i];
+    console.log(items);
+}
