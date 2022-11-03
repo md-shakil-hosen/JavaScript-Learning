49cf326826721fdc0388e6a563f4354e1d75e3fc
md-shakil-hosen <shakilinfo91@gmail.com>
2022-11-03 10:27:17 +0600

    For Loop

--- While-loop.js
+++ While-loop.js
@@ -4,5 +4,31 @@ while(roastGiven < 7){
     console.log("Roast Dan  Please!!");
     roastGiven ++;
     console.log(roastGiven);
+}
 
+var alphabet = 1; // loop variable
+while (alphabet <= 5){
+console.log(alphabet);
+alphabet++;
+}
+
+//Find odd Number
+
+var numberOdd = 1;
+var evenNumber = 0;
+while (numberOdd <=20 ){
+    console.log(numberOdd);
+    numberOdd = numberOdd + 2;
+}
+
+//Even Number
+while(evenNumber <=20 ){
+    console.log(evenNumber);
+    evenNumber = evenNumber + 2;
+}
+
+//For Loop
+
+for(roastGiven = 0; roastGiven <= 10; roastGiven++){
+    console.log("Mama Please Roast Dan");
 }
