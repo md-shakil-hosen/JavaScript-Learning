a3587ca4610b7272fd2edfc1d72ee4369fc0b0aa
md-shakil-hosen <shakilinfo91@gmail.com>
2022-11-05 13:22:32 +0600

    varaiable, Array

---
+++ Reacap-var-to-func.js
@@ -0,0 +1,24 @@
+//  Variable
+
+var a = 2;
+let b = "Shakil";
+const c = true;
+
+// String 
+
+var gfName = ["Sona", "moyna", "Pakhi"];
+var favNumber = [ 34., 45, 34, 84, 94, 44,];
+
+// add number from last -push() Method
+favNumber.push(45);
+// delete array in Pop() from last 
+favNumber.pop()
+// delete number in array with shift() Method 
+favNumber.shift()
+// add number unshift() method
+favNumber.unshift(50); 
+// check array index 
+console.log(favNumber.indexOf(44));
+// Add array Element
+
+
