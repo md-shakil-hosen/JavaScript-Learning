70d428d1689d7f6b2fc2972cca0435d6a81323f2
md-shakil-hosen <shakilinfo91@gmail.com>
2022-11-05 21:38:46 +0600

    division Number

--- multiple-parameter.js
+++ multiple-parameter.js
@@ -3,9 +3,30 @@
 var firstNumber = 100;
 var secondNumber = 200;
 function addTwoNumber (number1, number2){
-    console.log(number1, number2);
     var sum = number1 + number2;
     return sum;
 }
 var sumResult = addTwoNumber(firstNumber, secondNumber);
 console.log(sumResult);
+
+
+// Two  Multiplication  number
+
+var favNumber1 = 34;
+var favNumber2 =45
+function multiplicationTowNum(num1, num2){
+    var multipleCalculation = num1 * num2;
+    return multipleCalculation;
+}
+var totalCalculation = multiplicationTowNum(favNumber1, favNumber2);
+console.log(totalCalculation);
+
+
+// Division Two Number
+
+function twoNumberDivision(num1, num2){
+    var division = num1 / num2;
+    return division;
+}
+var divisionResult = twoNumberDivision(100, 50);
+console.log(divisionResult);
