2d27616a336ae01349abdaa4992acbf9c41c88f6
md-shakil-hosen <shakilinfo91@gmail.com>
2022-11-06 18:11:01 +0600

    JavaScript-Switch

---
+++ Javascript-switch.js
@@ -0,0 +1,18 @@
+var favColor = "";
+switch(favColor){
+    case 'Red':
+        console.log("it's red color");
+        break;
+    case 'Green':
+        console.log("it's green color");
+        break;
+    case 'Yellow':
+        console.log("it's Yellow color");
+        break;
+     default:
+        console.log("It's black Color");
+}
+
+
+// JavaScript Switch statement
+// সুইচ স্টেটমেন্টে কাজ করার জন্য আমাদের যা যা লাগবে, ১. একটি Variable এর মান , ২. ‍Switch Statement (Variable name)  ৩. case ‘ভ্যালু দিতে হবে এখানে’: ৪. কমেন্ট করতে হবে  । ৫. break 
