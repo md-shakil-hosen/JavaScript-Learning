9fa23803d5e162dab9f5f27a43460fc88a033e09
md-shakil-hosen <shakilinfo91@gmail.com>
2022-11-07 11:19:40 +0600

    break-continue

---
+++ break-continue.js
@@ -0,0 +1,19 @@
+// ব্রেক :  একটি নিদিষ্ট শর্ত পূর্ণ হওয়ার পর েব্রেক করে দিবে । 
+
+
+var i = 0
+while(i < 10){
+    console.log(i);
+    if(i == 5){
+        break;
+    }
+    i++;
+}
+
+while(i < 10){
+    console.log(i);
+    if(i == 10){
+        break;
+    }
+    i++;
+}
