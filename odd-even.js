278e8f050827ce4e93a7da17bc5ee230e3900844
md-shakil-hosen <shakilinfo91@gmail.com>
2022-11-09 18:50:49 +0600

    odd-even Number

--- odd-even.js
+++ odd-even.js
@@ -21,6 +21,17 @@ function  isOdd(number){
     return false;
 }
 
-const myFavNumber = 13;
-const checkOdd = isOdd(myFavNumber);
+// const myFavNumber = 13;
+// const checkOdd = isOdd(myFavNumber);
+// console.log(checkOdd);
+
+
+function isOdd1(number){
+    if(number % 2 !=0){
+        return true;
+    }
+    return false;
+}
+const myFavNumber = 12;
+const checkOdd = isOdd1(myFavNumber);
 console.log(checkOdd);
