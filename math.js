4791492ce0bedbef1cf8c3fbc4ba2b54a49689c7
md-shakil-hosen <shakilinfo91@gmail.com>
2022-11-10 16:00:30 +0600

    math

---
+++ math.js
@@ -0,0 +1,21 @@
+// 1. Math.random()
+// 2. Math.ceil()
+// 3. Math. round ()
+// 4. Math. flood ()
+
+
+const number = 20.25698;
+const randomNumber = Math.random(number) * 10;
+const ceilNumber = Math.ceil(number);
+const floorNumber = Math.floor(number);
+const roundedNumber = Math.round(number);
+console.log(randomNumber);
+console.log(ceilNumber);
+console.log(floorNumber);
+
+for(let i = 0; i <= 20; i++){
+const randomNumber = Math.random() * 10;
+const floorNumber2 = Math.floor(randomNumber);
+console.log(floorNumber2);
+}
+
