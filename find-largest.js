75fab76889b7659ff1cfa0e3941d8e2286fc2638
md-shakil-hosen <shakilinfo91@gmail.com>
2022-11-12 11:02:20 +0600

    FIND-LARGEST NUMBER

---
+++ find-largest.js
@@ -0,0 +1,13 @@
+// Find Largest Number in array 
+
+const numbers = [52, 45, 45, 52, 12];
+function findLargestNumber (numbers){
+    const max = numbers[0];
+    for(let i = 0; i < numbers.length; i++){
+        const elements = numbers[i];
+        if(elements < min);
+        min = elements;
+    }
+}
+
+findLargestNumber();
