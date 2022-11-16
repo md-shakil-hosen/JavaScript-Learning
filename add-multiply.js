27e5466b0144beffee5cd76f3626a82a809d8006
md-shakil-hosen <shakilinfo91@gmail.com>
2022-11-16 12:01:50 +0600

    wood calculation

--- add-multiply.js
+++ add-multiply.js
@@ -1,1 +1,19 @@
-console.log("Hello");
+/*
+১. টেবিল এর জন্য কাঠ লাগবে = 3cft
+২. চেয়ারের জন্য কাঠ লাগবে = 10 cft
+৩. খাটের জন্য কাঠ লাগবে = 50 cft
+*/
+
+function woodCalculator(chairQuantity, tableQuantity, bedQuantity){
+    const perChairWood = 3;
+    const perTableWood = 10;
+    const perBedWood = 50;
+    const chairWoodQuantity = chairQuantity * perChairWood;
+    const tableWoodQuantity = tableQuantity * perTableWood;
+    const bedWoodQuantity = bedQuantity * perBedWood;
+    const totalWood = chairWoodQuantity + tableWoodQuantity + bedWoodQuantity;
+    return totalWood;
+}
+
+const totalWoodCalculation = woodCalculator(1, 1, 1);
+console.log(totalWoodCalculation);
