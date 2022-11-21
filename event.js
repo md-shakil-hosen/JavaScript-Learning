eac1a7d49dda414f879319f92de90b9e4af4be08
md-shakil-hosen <shakilinfo91@gmail.com>
2022-11-21 11:17:54 +0600

    events

---
+++ event.js
@@ -0,0 +1,16 @@
+
+
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+
+}
+
+//Make green
+
+const greenButton = document.getElementById("make-green-button");
+
+greenButton.onclick = function(){
+    document.body.style.backgroundColor = 'green';
+}
+
+// Make Golden Rod
